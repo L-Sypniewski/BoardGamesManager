4a755e56037f848d6b410071a28147231bc0ff08
Łukasz Sypniewski <l.sypniewski@gmail.com>
2020-07-13 10:31:56 +0200

    Update PaginationLinqExtensions.cs
    Fixed wrong variable name

--- BoardGamesManager/BoardGamesManagerCore/Extensions/PaginationLinqExtensions.cs
+++ BoardGamesManager/BoardGamesManagerCore/Extensions/PaginationLinqExtensions.cs
@@ -11,10 +11,10 @@ namespace BoardGamesManagerCore.Extensions
             return pageSize.HasValue ? skippedQueryable.Take(pageSize.Value) : skippedQueryable;
         }
 
-        public static IEnumerable<T> Paginated<T>(this IEnumerable<T> queryable, int? pageSize = null, int? page = null)
+        public static IEnumerable<T> Paginated<T>(this IEnumerable<T> enumerable, int? pageSize = null, int? page = null)
         {
-            var skippedQueryable = queryable.Skip((page - 1 ?? 0) * (pageSize ?? 0));
-            return pageSize.HasValue ? skippedQueryable.Take(pageSize.Value) : skippedQueryable;
+            var skippedEnumerable = enumerable.Skip((page - 1 ?? 0) * (pageSize ?? 0));
+            return pageSize.HasValue ? skippedEnumerable.Take(pageSize.Value) : skippedEnumerable;
         }
     }
 }
